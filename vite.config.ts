880dd0645cf0cc97994c729cd4989846f212f334
Boyan <boyan@increo.no>
2026-04-07 09:18:26 +0200

    Removed https true

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  server: { https: true, host: true }
-})
+  server: { https: {}, host: true },
+});
